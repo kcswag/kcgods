ab9c3a13f65cfc42b321cadafeff19cf710eecd4
kinson chow <kcswag@foxmail.com>
2022-07-05 08:57:35 +0800

    add concurrenthashmap

--- maps/concurrenthashmap/concurrenthashmap.go
+++ maps/concurrenthashmap/concurrenthashmap.go
@@ -2,22 +2,24 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package hashmap implements a map backed by a hash table.
+// Package concurrenthashmap  implements a map backed by a hash table.
 //
 // Elements are unordered in the map.
 //
-// Structure is not thread safe.
+// Structure is thread safe.
 //
 // Reference: http://en.wikipedia.org/wiki/Associative_array
-package hashmap
+package concurrenthashmap
 
 import (
 	"fmt"
+	"sync"
 )
 
 // Map holds the elements in go's native map
 type Map[K comparable, V any] struct {
 	m map[K]V
+	sync.RWMutex
 }
 
 // New instantiates a hash map.
@@ -27,19 +29,28 @@ func New[K comparable, V any]() *Map[K, V] {
 
 // Put inserts element into the map.
 func (m *Map[K, V]) Put(key K, value V) {
-	m.m[key] = value
+	if m.TryLock() {
+		defer m.Unlock()
+		m.m[key] = value
+	}
 }
 
 // Get searches the element in the map by key and returns its value or nil if key is not found in map.
 // Second return parameter is true if key was found, otherwise false.
 func (m *Map[K, V]) Get(key K) (value V, found bool) {
-	value, found = m.m[key]
+	if m.TryRLock() {
+		defer m.RUnlock()
+		value, found = m.m[key]
+	}
 	return
 }
 
 // Remove removes the element from the map by key.
 func (m *Map[K, V]) Remove(key K) {
-	delete(m.m, key)
+	if m.TryLock() {
+		defer m.Unlock()
+		delete(m.m, key)
+	}
 }
 
 // Empty returns true if map does not contain any elements
@@ -76,7 +87,10 @@ func (m *Map[K, V]) Values() []V {
 
 // Clear removes all elements from the map.
 func (m *Map[K, V]) Clear() {
-	m.m = make(map[K]V)
+	if m.TryLock() {
+		defer m.Unlock()
+		m.m = make(map[K]V)
+	}
 }
 
 // String returns a string representation of container
